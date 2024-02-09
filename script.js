68e5cd3fdff79259f490daccb208a0f59a28a083
wasim2002 <wansari4540@gmail.com>
2024-02-09 23:32:21 +0530

    add js file

---
+++ script.js
@@ -0,0 +1,35 @@
+const inputField = document.querySelector("#inputF")
+function getelem(el) {
+    console.log(el.innerHTML);
+}
+function ans() {
+
+}
+
+
+// const allB = document.querySelectorAll(".b")
+    // allB.forEach((b) => {
+    //     b.addEventListener("click", (e) => {
+    //         if (e.target.classList[0] == "fa-solid") {
+    //             if (e.target.classList[1] == "fa-divide") {
+    //                 inputField.value += "/"
+    //             }
+    //             else if (e.target.classList[1] == "fa-xmark") {
+    //                 inputField.value += "*"
+    //             }
+    //             else if (e.target.classList[1] == "fa-minus") {
+    //                 inputField.value += " - "
+    //             }
+    //             else if (e.target.classList[1] == "fa-plus") {
+    //                 inputField.value += "+"
+    //             }
+    //             else if (e.target.classList[1] == "fa-equals") {
+    //                 eval = eval(inputValue)
+    //                 console.log(eval);
+    //             }
+    //         } else {
+    //             inputField.value += e.target.innerText
+    //             inputValue = inputField.value
+    //         }
+    //     })
+    // })
