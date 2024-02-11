0996730f36f8fcabbb6bed482ea231d5455fbba6
wasim2002 <wansari4540@gmail.com>
2024-02-11 21:58:29 +0530

    add some js lines

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 const inputField = document.querySelector("#inputF")
-function getelem(el) {
-    console.log(el.innerHTML);
-}
-function ans() {
-
-}
-
-
-// const allB = document.querySelectorAll(".b")
-    // allB.forEach((b) => {
-    //     b.addEventListener("click", (e) => {
-    //         if (e.target.classList[0] == "fa-solid") {
-    //             if (e.target.classList[1] == "fa-divide") {
-    //                 inputField.value += "/"
-    //             }
-    //             else if (e.target.classList[1] == "fa-xmark") {
-    //                 inputField.value += "*"
-    //             }
-    //             else if (e.target.classList[1] == "fa-minus") {
-    //                 inputField.value += " - "
-    //             }
-    //             else if (e.target.classList[1] == "fa-plus") {
-    //                 inputField.value += "+"
-    //             }
-    //             else if (e.target.classList[1] == "fa-equals") {
-    //                 eval = eval(inputValue)
-    //                 console.log(eval);
-    //             }
-    //         } else {
-    //             inputField.value += e.target.innerText
-    //             inputValue = inputField.value
-    //         }
-    //     })
-    // })
+const allB = document.querySelectorAll(".b")
+let symbole = true
+allB.forEach((b) => {
+    b.addEventListener("click", (e) => {
+        if (e.target.classList[0] == "fa-solid") {
+            if (e.target.classList[1] == "fa-divide" && symbole == true) {
+                inputField.value += "/"
+                symbole = false
+            }
+            else if (e.target.classList[1] == "fa-xmark" && symbole == true) {
+                let xSentax = x
+                inputField.value += `${xSentax}`
+                symbole = false
+            }
+            else if (e.target.classList[1] == "fa-minus" && symbole == true) {
+                inputField.value += " - "
+                symbole = false
+            }
+            else if (e.target.classList[1] == "fa-plus" && symbole == true) {
+                inputField.value += "+"
+                symbole = false
+            }
+            else if (e.target.classList[1] == "fa-equals" && symbole == true) {
+                eval = eval(inputValue)
+                console.log(eval);
+                symbole = false
+            }
+        } else {
+            symbole = true
+            inputField.value += e.target.innerText
+            inputValue = inputField.value
+        }
+    })
+})
