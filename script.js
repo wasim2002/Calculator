7733fff00bbe517826f3d5716535685ac5c9dae8
wasim2002 <wansari4540@gmail.com>
2024-02-12 17:12:58 +0530

    add calculate method and all clear

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const inputField = document.querySelector("#inputF")
+const ansField = document.querySelector("#answer")
 const allB = document.querySelectorAll(".b")
 let symbole = true
 allB.forEach((b) => {
     b.addEventListener("click", (e) => {
-        if (e.target.classList[0] == "fa-solid") {
+        if (e.target.classList[2] == "AC") {
+            inputField.value = ""
+            ansField.value = ""
+        } else if (e.target.classList[0] == "fa-solid") {
             if (e.target.classList[1] == "fa-divide" && symbole == true) {
                 inputField.value += "/"
                 symbole = false
             }
             else if (e.target.classList[1] == "fa-xmark" && symbole == true) {
-                let xSentax = x
-                inputField.value += `${xSentax}`
+                inputField.value += "*"
                 symbole = false
             }
             else if (e.target.classList[1] == "fa-minus" && symbole == true) {
@@ -23,7 +26,7 @@ allB.forEach((b) => {
             }
             else if (e.target.classList[1] == "fa-equals" && symbole == true) {
                 eval = eval(inputValue)
-                console.log(eval);
+                ansField.value = eval
                 symbole = false
             }
         } else {
