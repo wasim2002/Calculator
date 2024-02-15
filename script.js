55c300b6d02fcb7d10d59db1c0b0d7e522af5e0b
wasim2002 <wansari4540@gmail.com>
2024-02-15 23:24:42 +0530

    complete the code

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-const inputField = document.querySelector("#inputF")
-const ansField = document.querySelector("#answer")
+const inputField = document.querySelector("#input")
 const allB = document.querySelectorAll(".b")
 let symbole = true
+let inputValue = ""
 allB.forEach((b) => {
     b.addEventListener("click", (e) => {
         if (e.target.classList[2] == "AC") {
-            inputField.value = ""
-            ansField.value = ""
-        } else if (e.target.classList[0] == "fa-solid") {
+            inputValue = ""
+            inputField.value = inputValue
+        }
+        else if (e.target.classList[2] == "C") {
+            inputValue = inputValue.substring(0, inputValue.length - 1)
+            inputField.value = inputValue
+        }
+        else if (e.target.classList[0] == "fa-solid") {
             if (e.target.classList[1] == "fa-divide" && symbole == true) {
-                inputField.value += "/"
+                inputValue += "/"
+                inputField.value = inputValue
                 symbole = false
             }
             else if (e.target.classList[1] == "fa-xmark" && symbole == true) {
-                inputField.value += "*"
+                inputValue += "*"
+                inputField.value = inputValue
                 symbole = false
             }
             else if (e.target.classList[1] == "fa-minus" && symbole == true) {
-                inputField.value += " - "
+                inputValue += "-"
+                inputField.value = inputValue
                 symbole = false
             }
             else if (e.target.classList[1] == "fa-plus" && symbole == true) {
-                inputField.value += "+"
+                inputValue += "+"
+                inputField.value = inputValue
                 symbole = false
             }
             else if (e.target.classList[1] == "fa-equals" && symbole == true) {
-                eval = eval(inputValue)
-                ansField.value = eval
+                inputValue = eval(inputValue)
+                inputField.value = inputValue
                 symbole = false
             }
         } else {
             symbole = true
-            inputField.value += e.target.innerText
-            inputValue = inputField.value
+            inputValue += e.target.innerText
+            inputField.value = inputValue
         }
     })
 })
